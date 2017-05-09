388e24b8fe03ca7a947353f33dfdb9d444349427
Vulcan <vulcan.lee@gmail.com>
2017-05-09 12:04:06 +0800

    Xamarin.iOS 專案練習原始碼

---
+++ XISingle/XISingle/ViewController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using UIKit;
+
+namespace XISingle
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            fooButton.TouchUpInside += (s, e) =>
+            {
+                fooLabel.Text =
+                "Hello iOS, " + FooTextField.Text;
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+    }
+}
