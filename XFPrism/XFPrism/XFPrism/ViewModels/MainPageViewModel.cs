9801b139f189608dd0b820c0ba3ecc37d4892884
Vulcan <vulcan.lee@gmail.com>
2017-05-09 14:35:46 +0800

    使用 Prism 來建立 Xamarin.forms 專案原始碼

---
+++ XFPrism/XFPrism/XFPrism/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFPrism.ViewModels
+{
+
+    [ImplementPropertyChanged]
+    public class MainPageViewModel :  INavigationAware
+    {
+        public string Title { get; set; }
+        public string fooEntry { get; set; }
+        public string fooLabel { get; set; }
+        public DelegateCommand OKCommand { get; set; }
+        public MainPageViewModel()
+        {
+            OKCommand = new DelegateCommand(() =>
+            {
+                fooLabel = "Hi Prism, " + fooEntry;
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
