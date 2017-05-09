5f09e2a1f17a2fb3e897ead7575b313d280b0e53
Vulcan <vulcan.lee@gmail.com>
2017-05-09 13:37:52 +0800

    Xamarin.Forms 開發練習

---
+++ XFSingle/XFSingle/XFSingle/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFSingle
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            fooButton.Clicked +=
+                (s, e) =>
+                {
+                    fooLabel.Text =
+                    "Hello XF, " +
+                    fooEntry.Text;
+                };
+        }
+    }
+}
