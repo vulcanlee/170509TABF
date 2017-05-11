92d5a7a1e8f3421dc6d33444f728f7338f954cc5
Vulcan <vulcan.lee@gmail.com>
2017-05-11 14:35:06 +0800

    導航抽屜

---
+++ XFMasterDetail/XFMasterDetail/XFMasterDetail/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Unity;
+using XFMasterDetail.Views;
+using Xamarin.Forms;
+
+namespace XFMasterDetail
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MDPage/NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+            //NavigationService.NavigateAsync("MDPage/NaviPage/P2Page");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+            Container.RegisterTypeForNavigation<NextPage>();
+            Container.RegisterTypeForNavigation<P1Page>();
+            Container.RegisterTypeForNavigation<P2Page>();
+            Container.RegisterTypeForNavigation<NaviPage>();
+            Container.RegisterTypeForNavigation<MDPage>();
+        }
+    }
+}
---
+++ XFMasterDetail/XFMasterDetail/XFMasterDetail/Views/P2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XFMasterDetail.Views
+{
+    public partial class P2Page : ContentPage
+    {
+        public P2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
