8d3ee0646fb0554af945159677baa98706f612d6
Vulcan <vulcan.lee@gmail.com>
2017-05-11 08:55:07 +0800

    使用XAML來顯示這個頁面內容

---
+++ XFXAML/XFXAML/XFXAML/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Unity;
+using XFXAML.Views;
+using Xamarin.Forms;
+
+namespace XFXAML
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
