ca82f3d47ee75c57f6e5d2678e47d036f7a9498f
Vulcan <vulcan.lee@gmail.com>
2017-05-11 12:02:56 +0800

    頁面導航與傳遞參數

---
+++ XFNaviPara/XFNaviPara/XFNaviPara/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,97 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFNaviPara.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+
+        #region entry1
+        private string _entry1;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public string entry1
+        {
+            get { return this._entry1; }
+            set { this.SetProperty(ref this._entry1, value); }
+        }
+        #endregion
+
+        #region entry2
+        private int _entry2;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public int entry2
+        {
+            get { return this._entry2; }
+            set { this.SetProperty(ref this._entry2, value); }
+        }
+        #endregion
+
+        #region entry3
+        private int _entry3;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public int entry3
+        {
+            get { return this._entry3; }
+            set { this.SetProperty(ref this._entry3, value); }
+        }
+        #endregion
+
+
+        public DelegateCommand NextPageCommand { get; set; }
+
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private readonly INavigationService _navigationService;
+        public MainPageViewModel(INavigationService navigationService)
+        {
+
+            _navigationService = navigationService;
+
+            NextPageCommand = new DelegateCommand(() =>
+            {
+                // 使用 NavigationParameters
+                var fooNaviPara = new NavigationParameters();
+                fooNaviPara.Add("value1", entry2);
+                fooNaviPara.Add("value2", entry3);
+
+                // 使用 QueryString
+                var fooQString = "?Name=" + entry1;
+                // 開始導航與傳入引數
+                _navigationService.
+                NavigateAsync("NextPage"+ fooQString, fooNaviPara);
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
---
+++ XFNaviPara/XFNaviPara/XFNaviPara/ViewModels/NextPageViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Prism.Navigation;
+
+namespace XFNaviPara.ViewModels
+{
+    public class NextPageViewModel : BindableBase, INavigationAware
+    {
+
+
+        #region label1
+        private string _label1;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public string label1
+        {
+            get { return this._label1; }
+            set { this.SetProperty(ref this._label1, value); }
+        }
+        #endregion
+
+        #region labe2
+        private int _label2;
+        /// <summary>
+        /// PropertyDescription
+        /// </summary>
+        public int label2
+        {
+            get { return this._label2; }
+            set { this.SetProperty(ref this._label2, value); }
+        }
+        #endregion
+
+        public NextPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if(parameters.ContainsKey("Name"))
+            {
+                label1 = parameters["Name"] as string;
+            }
+            int v1=0,v2=0;
+            if (parameters.ContainsKey("value1"))
+            {
+                v1 = (int)parameters["value1"];
+            }
+            if (parameters.ContainsKey("value2"))
+            {
+                v2 = (int)parameters["value2"];
+            }
+            label2 = v1 + v2;
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+        }
+    }
+}
